32b182258eeae52ce8247f7c5e8265467e9bb0be
LaQuan <la'quan.smith@selu.edu>
2022-09-06 15:34:28 -0500

    modelBuilder Entities

--- FA22.P03.Web/Data/DataContext.cs
+++ FA22.P03.Web/Data/DataContext.cs
@@ -1,7 +1,6 @@
-﻿
-using FA22.P03.Web.Models;
+﻿using FA22.P03.Web.Models;
 using Microsoft.EntityFrameworkCore;
-using System.Security.Claims;
+
 
 namespace FA22.P03.Web.Data
 {
@@ -16,6 +15,59 @@ namespace FA22.P03.Web.Data
         public DbSet<Item> Items { get; set; }
         public DbSet<ItemListing> ItemListings { get; set; }
         public DbSet<Listing> Listings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(x => x.Id)
+                .IsRequired();
+
+            modelBuilder.Entity<Product>()
+               .Property(x => x.Name)
+               .IsRequired();
+
+            modelBuilder.Entity<Product>()
+               .Property(x => x.Description)
+               .IsRequired();
+
+            modelBuilder.Entity<Product>()
+               .Property(x => x.Name)
+               .IsRequired();
+
+            modelBuilder.Entity<Item>()
+                .Property(x => x.Id)
+                .IsRequired();
+
+            modelBuilder.Entity<Item>()
+               .Property(x => x.Product)
+               .IsRequired();
+
+            modelBuilder.Entity<Item>()
+              .Property(x => x.Conditon)
+              .IsRequired();
+
+            modelBuilder.Entity<Listing>()
+             .Property(x => x.Id)
+             .IsRequired();
+
+            modelBuilder.Entity<Listing>()
+            .Property(x => x.Name)
+            .IsRequired();
+
+            modelBuilder.Entity<Listing>()
+            .Property(x => x.Price)
+            .IsRequired();
+
+            modelBuilder.Entity<Listing>()
+            .Property(x => x.StartUtc)
+            .IsRequired();
+
+            modelBuilder.Entity<Listing>()
+            .Property(x => x.EndUtc)
+            .IsRequired();
+
+        }
+
+
     }
-    
 }
